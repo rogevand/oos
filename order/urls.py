1b36df7bcb3d9284079d5ebdce21b7d71a7bbcb3
rogevand <rvand516@gmail.com>
2021-04-11 13:20:57 -0400

    updated

--- order/urls.py
+++ order/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:service_id>/', views.history, name='history')
+    path('<int:service_id>/', views.history, name='history'),
+    path('addservice/', views.addservice, name='addservice'),
+    path('great/', views.great, name='great'),
+
+    
 ]
--- order/views.py
+++ order/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Service
 from django.template import context
+from .forms import NameForm
 
 
 def index(request):
@@ -14,3 +15,20 @@ def index(request):
 def history(request, service_id):
     return  HttpResponse("you are looking at the %s service " % service_id)
 
+def addservice(request):
+    #service = get_object_or_404(Service, pk=service_id)
+    if request.method == 'POST':
+        form = NameForm(request.POST)
+        if form.is_valid():
+            pass
+            # redirect to a new URL:
+            #return HttpResponseRedirect('/thanks/')
+    else:
+        form = NameForm()
+    context = {'form': form}
+    return render(request, 'order/addservice.html', context)
+
+
+def great(request):
+    context = {great: "great"}
+    return render(request, 'order/great.html', context)
