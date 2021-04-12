5d77f8a15eba651a1aad87f92bc28540d7ae7856
rogevand <rvand516@gmail.com>
2021-04-11 22:38:03 -0400

    idk

--- order/urls.py
+++ order/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.base, name='base'),
+    path('base/', views.base, name='base'),
     path('history/', views.history, name='history'),
     path('addservice/', views.addservice, name='addservice'),
     path('great/', views.great, name='great'),
